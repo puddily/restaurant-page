1a716baeb4d4ce2e35970b874dfe35deaf2a438d
plrpl <hallowed@live.com>
2023-05-21 09:07:06 -0500

    Start adding header to index.js to move to building webpage using JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello webpack!");
+console.log("Hello webpack!")
+
+let content = document.querySelector('#content')
+//Nav
+const header = document.createElement('header')
+const nav = document.createElement('nav')
+const ul = document.createElement('ul')
+const liHome = document.createElement('li')
+const liMenu = document.createElement('li')
+const liContact = document.createElement('li')
+ul.appendChild(liHome)
+ul.appendChild(liMenu)
+ul.appendChild(liContact)
+nav.appendChild(ul)
+header.appendChild(nav)
+content.appendChild(header)
+
+{/* <header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</header> */}

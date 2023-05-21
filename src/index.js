14bd9b40b331ceb81cc2024e789aed92d7176a2d
plrpl <hallowed@live.com>
2023-05-21 09:10:08 -0500

    Add header via JS

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ const header = document.createElement('header')
 const nav = document.createElement('nav')
 const ul = document.createElement('ul')
 const liHome = document.createElement('li')
+liHome.textContent = "Home"
 const liMenu = document.createElement('li')
+liMenu.textContent = "Menu"
 const liContact = document.createElement('li')
+liContact.textContent = "Contact"
 ul.appendChild(liHome)
 ul.appendChild(liMenu)
 ul.appendChild(liContact)
 nav.appendChild(ul)
 header.appendChild(nav)
-content.appendChild(header)
+content.prepend(header)
 
 {/* <header>
     <nav>

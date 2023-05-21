27248127c8f6eb8c84182bbd1fa3aa95f4937230
plrpl <hallowed@live.com>
2023-05-21 09:14:44 -0500

    Move navbar to its own module

---
+++ src/header.js
@@ -0,0 +1,17 @@
+export function buildHeader(){
+    const header = document.createElement('header')
+    const nav = document.createElement('nav')
+    const ul = document.createElement('ul')
+    const liHome = document.createElement('li')
+    liHome.textContent = "Home"
+    const liMenu = document.createElement('li')
+    liMenu.textContent = "Menu"
+    const liContact = document.createElement('li')
+    liContact.textContent = "Contact"
+    ul.appendChild(liHome)
+    ul.appendChild(liMenu)
+    ul.appendChild(liContact)
+    nav.appendChild(ul)
+    header.appendChild(nav)
+    return header
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
+import { buildHeader } from "./header"
 console.log("Hello webpack!")
 
 let content = document.querySelector('#content')
-//Nav
-const header = document.createElement('header')
-const nav = document.createElement('nav')
-const ul = document.createElement('ul')
-const liHome = document.createElement('li')
-liHome.textContent = "Home"
-const liMenu = document.createElement('li')
-liMenu.textContent = "Menu"
-const liContact = document.createElement('li')
-liContact.textContent = "Contact"
-ul.appendChild(liHome)
-ul.appendChild(liMenu)
-ul.appendChild(liContact)
-nav.appendChild(ul)
-header.appendChild(nav)
-content.prepend(header)
+content.prepend(buildHeader());
+
 
 {/* <header>
     <nav>

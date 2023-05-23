dbc932a4eee06276eff2d7a9a271522ae5ce0e7f
plrpl <hallowed@live.com>
2023-05-22 21:04:28 -0500

    Only render home page if currentTab is equal to home

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import { buildHeader } from "./header"
 import { buildFooter } from "./footer";
 import { buildHero, buildAbout, buildMenuAction } from "./about";
 console.log("Hello webpack!")
+let currentTab = "Home"
 
 let content = document.querySelector('#content')
-content.prepend(buildHeader());
 let main = document.querySelector("main");
-main.prepend(buildMenuAction());
-main.prepend(buildAbout());
-main.prepend(buildHero());
+content.prepend(buildHeader());
+if(currentTab === "Home"){
+    main.prepend(buildMenuAction());
+    main.prepend(buildAbout());
+    main.prepend(buildHero());
+}
 main.appendChild(buildFooter());

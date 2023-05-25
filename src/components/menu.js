c295420b4b640c11af1ca278dce3d7825ceadac4
plrpl <hallowed@live.com>
2023-05-25 08:36:17 -0500

    Build menu page using JS

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,76 @@
-export function buildMenuAction(){
+export function buildMenu(){
+    let menuSection = document.createElement('section')
+    menuSection.id = "menu"
 
+    let menuHeader = document.createElement('h2')
+    menuHeader.id = 'menu-header'
+    menuHeader.textContent = 'Cozy Bean Café Menu'
+
+    let menuCardsContainer = document.createElement('div')
+    menuCardsContainer.classList.add('menu-cards')
+
+    let menuCard1 = buildMenuCard(
+        'Cozy Bean Signature Blend',
+        'Our house specialty, a carefully crafted blend of premium coffee beans, resulting in a smooth and rich flavor profile.',
+        '$3.50'
+    )
+    let menuCard2 = buildMenuCard(
+        'Café Latte',
+        'Smooth espresso combined with velvety steamed milk, creating a creamy and comforting coffee delight.',
+        '$4.50'
+    )
+    let menuCard3 = buildMenuCard(
+        'Classic Espresso',
+        'A rich and bold shot of espresso, expertly brewed to perfection.',
+        '$3.50'
+    )
+    let menuCard4 = buildMenuCard(
+        'Americano',
+        'A strong and robust shot of espresso diluted with hot water, providing a bold and invigorating coffee experience.',
+        '$3.50'
+    )
+    let menuCard5 = buildMenuCard(
+        'Cappuccino',
+        'Equal parts espresso, steamed milk, and frothed milk, resulting in a perfectly balanced coffee with a fluffy foam topping.',
+        '$4.50'
+    )
+
+    menuCardsContainer.append(menuCard1)
+    menuCardsContainer.append(menuCard2)
+    menuCardsContainer.append(menuCard3)
+    menuCardsContainer.append(menuCard4)
+    menuCardsContainer.append(menuCard5)
+
+    menuSection.append(menuHeader)
+    menuSection.append(menuCardsContainer)
+    return menuSection
+
+}   
+
+function buildMenuCard(itemName, description, price){
+    let menuCard = document.createElement('div')
+    menuCard.classList.add('menu-card')
+    let menuCardImg = document.createElement('img')
+    menuCardImg.setAttribute('src', './images/coffee-4752461.svg')
+    menuCardImg.setAttribute('width', '100px')
+
+    let menuCardInfoContainer = document.createElement('div')
+    menuCardInfoContainer.classList.add('card-info')
+    let menuCardInfoHeader = document.createElement('h3')
+    menuCardInfoHeader.textContent = itemName
+    let menuCardInfoDescription = document.createElement('p')
+    menuCardInfoDescription.textContent = description
+    let menuCardInfoPrice = document.createElement('p')
+    menuCardInfoPrice.textContent = price
+
+    menuCardInfoContainer.appendChild(menuCardInfoHeader)
+    menuCardInfoContainer.appendChild(menuCardInfoDescription)
+    menuCardInfoContainer.appendChild(menuCardInfoPrice)
+
+    menuCard.appendChild(menuCardImg)
+    menuCard.appendChild(menuCardInfoContainer)
+
+    return menuCard;
 }
 
 {/* <h1>Cozy Bean Café</h1>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { buildHeader } from "./components/header"
 import { buildFooter } from "./components/footer";
 import { buildHero, buildAbout, buildMenuAction } from "./components/about";
+import { buildMenu } from "./components/menu";
 console.log("Hello webpack!")
 let currentTab = "Menu" //"Home", "Contact", "Menu"
 
@@ -11,5 +12,11 @@ if(currentTab === "Home"){
     main.prepend(buildMenuAction());
     main.prepend(buildAbout());
     main.prepend(buildHero());
+}
+if(currentTab === "Menu"){
+    let topHeader = document.createElement('h1');
+    topHeader.textContent = "Cozy Bean Cafe" 
+    main.prepend(topHeader);
+    main.prepend(buildMenu());
 }
 main.appendChild(buildFooter());

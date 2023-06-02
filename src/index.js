2f9bc51bbeb10e2579249cffbdcec83e6afbd3ac
plrpl <hallowed@live.com>
2023-06-01 19:11:58 -0500

    Modularize building of entire website with tab as parameter

--- src/index.js
+++ src/index.js
@@ -4,28 +4,43 @@ import { buildHero, buildAbout, buildMenuAction } from "./components/about";
 import { buildMenu } from "./components/menu";
 import { buildContact } from "./components/contact";
 console.log("Hello webpack!")
-let currentTab = "Contact" //"Home", "Contact", "Menu"
+function buildSite(tab){ //"Home", "Contact", "Menu"
+    let currentTab = tab
+    let content = document.querySelector('#content')
 
-let content = document.querySelector('#content')
-let main = document.querySelector("main");
-content.prepend(buildHeader());
-if(currentTab === "Home"){
-    main.prepend(buildMenuAction());
-    main.prepend(buildAbout());
-    main.prepend(buildHero());
+    content.replaceChildren(); //Clear content
+
+    let newMain = document.createElement('main') //Readd "main" tag
+    content.appendChild(newMain);
+    let main = content.querySelector("main");
+
+
+    content.prepend(buildHeader());
+    if(currentTab === "Home"){
+        main.prepend(buildMenuAction());
+        main.prepend(buildAbout());
+        main.prepend(buildHero());
+    }
+    if(currentTab === "Menu"){
+        //TODO: Move topheader to module
+        let topHeader = document.createElement('h1');
+        topHeader.textContent = "Cozy Bean Cafe" 
+        main.prepend(topHeader);
+        main.prepend(buildMenu());
+    }
+    if(currentTab === "Contact"){
+        //TODO: Move topheader to module
+        let topHeader = document.createElement('h1');
+        topHeader.textContent = "Cozy Bean Cafe" 
+        main.prepend(topHeader);
+        main.prepend(buildContact());
+    }
+    main.appendChild(buildFooter());
 }
-if(currentTab === "Menu"){
-    //TODO: Move topheader to module
-    let topHeader = document.createElement('h1');
-    topHeader.textContent = "Cozy Bean Cafe" 
-    main.prepend(topHeader);
-    main.prepend(buildMenu());
+
+function swapTab(){
+
+
 }
-if(currentTab === "Contact"){
-    //TODO: Move topheader to module
-    let topHeader = document.createElement('h1');
-    topHeader.textContent = "Cozy Bean Cafe" 
-    main.prepend(topHeader);
-    main.prepend(buildContact());
-}
-main.appendChild(buildFooter());
+
+buildSite("Home");
